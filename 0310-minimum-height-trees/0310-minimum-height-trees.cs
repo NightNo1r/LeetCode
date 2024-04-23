e4bdeacffc72bbe09bb756492d34705385f63cd8
Artem <57496218+NightNo1r@users.noreply.github.com>
2024-04-24 01:09:02 +0300

    Time: 299 ms (69.9%), Space: 71.5 MB (84.18%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cs
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cs
@@ -1,94 +1,58 @@
 public class Solution {
     public IList<int> FindMinHeightTrees(int n, int[][] edges)
     {
-        var graph = new Graph(n, edges);
-
-        var excludedNodes = new bool[graph.Nodes.Length];
-        while(true)
+        if(n == 1)
         {
-            var nodesToExclude = graph.Nodes
-                .Where(x => x.Neighbours.Count == 1)
-                .ToArray();
+            return new[] { 0 };
+        }
 
-            if(nodesToExclude.Length == 0)
+        var graph = new Dictionary<int, List<int>>();
+
+        for(var i = 0; i < n; i++)
+        {
+            graph.Add(i, new List<int>());
+        }
+
+        for(var i = 0; i < edges.Length; i++)
+        {
+            graph[edges[i][0]].Add(edges[i][1]);
+            graph[edges[i][1]].Add(edges[i][0]);
+        }
+
+        var incomingEdges = new int[n];
+        var leafs = new Queue<int>();
+        for(var i = 0; i < n; i++)
+        {
+            incomingEdges[i] = graph[i].Count;
+
+            if(incomingEdges[i] == 1)
             {
-                break;
+                leafs.Enqueue(i);
             }
+        }
 
-            if(nodesToExclude.Length == 2 && nodesToExclude[0].Neighbours[0].Index == nodesToExclude[1].Index)
-            {
-                break;
-            }
+        while(n > 2)
+        {
+            var leafCount = leafs.Count;
+            n -= leafCount;
 
-            foreach(var node in nodesToExclude)
+            while(leafCount > 0)
             {
-                if(excludedNodes[node.Index] == false)
+                var leaf = leafs.Dequeue();
+
+                foreach(var neighbour in graph[leaf])
                 {
-                    node.Neighbours[0].RemoveNeighbour(node.Index);
-                    node.RemoveNeighbour(node.Neighbours[0].Index);
+                    incomingEdges[neighbour]--;
+                    if(incomingEdges[neighbour] == 1)
+                    {
+                        leafs.Enqueue(neighbour);
+                    }
                 }
 
-                excludedNodes[node.Index] = true;
+                leafCount--;
             }
         }
 
-        var result = new List<int>();
-
-        for(var i = 0; i < excludedNodes.Length; i++)
-        {
-            if(excludedNodes[i] == true)
-            {
-                continue;
-            }
-
-            result.Add(i);
-        }
-
-        return result;
-    }
-
-    private class Graph
-    {
-        public Node[] Nodes { get; }
-
-        public Graph(int n, int[][] edges)
-        {
-            Nodes = new Node[n];
-
-            for(var i = 0; i < n; i++)
-            {
-                Nodes[i] = new Node(i);
-            }
-
-            for(var i = 0; i < edges.Length; i++)
-            {
-                Nodes[edges[i][0]].AddNeighbour(Nodes[edges[i][1]]);
-                Nodes[edges[i][1]].AddNeighbour(Nodes[edges[i][0]]);
-            }
-        }
-    }
-    
-    private class Node
-    {
-        private readonly List<Node> _neighbours;
-
-        public int Index { get; }
-        public IReadOnlyList<Node> Neighbours => _neighbours;
-
-        public Node(int index)
-        {
-            Index = index;
-            _neighbours = new List<Node>();
-        }
-
-        public void AddNeighbour(Node node)
-        {
-            _neighbours.Add(node);
-        }
-
-        public void RemoveNeighbour(int index)
-        {
-            _neighbours.RemoveAll(x => x.Index == index);
-        }
+        return leafs.ToList();
     }
 }
